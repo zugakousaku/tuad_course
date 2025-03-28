23a706c8f7e1335b6891cb1f7377dbe16cf20197
Mamoru Kano <zuga@zugakousaku.com>
2025-03-28 11:30:47 +0900

    Ttile

--- html_character_game/index.php
+++ html_character_game/index.php
@@ -8,197 +8,23 @@
 </header> -->
 
 <div class="container" >
-<div class="row p-4 mx-4">
-<div class="col-lg-6 ">
-  <img src="img/start/title.png">
+<div class="row">
+<div class="col-12 d-none d-md-block"><img src="img/start/title.svg"></div>
+<div class="col-12 d-block d-md-none"><img src="img/start/title_sm.svg"></div>
 </div>
 </div>
-
 
 <!-- ========= Article ========== -->
-<div class="container p-0"><div class="row rounded-3 bg-light p-4 m-0 my-4 m-lg-4">
-
-<div class="col-md-5">
-<h2>ようこそ映像学科へ</h2>
+<div class="container">
+<div class="row">
+<div class="col-md-7 text-light">
+  <h4 class="text-light">キャラクターを作り出し、体験をデザインする</h4>
+<p>キャラクターとゲームデザインの基礎を学び、キャラクターの社会的な活用方法と、実践的なゲーム開発スキルを習得します。社会のニーズを捉えながら、文化やビジネスといった大学ならではの幅広い知識をもとに、キャラクターを具体化。現実感のある世界観を構築しましょう。</p>
 </div>
 
-<div class="col-md-7">
-  <h4>入学おめでとうございます！</h4>
-<p>これまでの努力が身を結び、新たな4年間の学びの冒険がスタートしますね。映像学科では、皆さんがより良いキャンパスライフを送れるよう、2つの課題を準備しました。</p>
-<p>これから、映像を「デザイン」することへの関心を高め、受け手から作り手に思考をアップデートする必要があります。表現に対して「なぜだろう？」という問いを持つことで、あなた自身の好奇心があなたを新しい世界へ導いてくれるはずです。</p>
-<p>この二つの課題はそうした好奇心を生み出すきっかけとなるはずです。しっかりと取り組んでください。成果を楽しみにしています。</p>
-<p>映像学科 教員一同</p>
+</div>
 </div>
 
-</div></div>
-
-
-<!-- ========= Article ========== -->
-<div class="container p-0"><div class="row rounded-3 bg-light p-4 m-0 my-4 m-lg-4">
-
-<div class="col-md-5">
-<h2>課題一覧</h2>
-</div>
-
-<div class="col-md-7">
-<p>
-  <ul class="list-group list-group-flush">
-  <li class="list-group-item ps-0"><span class=" badge rounded-pill bg-info me-2 fs-7 px-2">課題1</span>映画と展覧会の鑑賞</li>
-  <li class="list-group-item ps-0"><span class=" badge rounded-pill bg-info me-2 fs-7 px-2">課題2</span>自己紹介ビデオの制作</li>
-</ul>
-</p>
-<p class="fw-bold fs-5">
-  締め切り：2025年3月30日(日)23:59
-</p>
-<p>
-  それぞれの課題説明にあるフォームへのリンクボタンから提出してください。
-</p>
-
-
-</div>
-</div></div>
-
-
-<!-- ========= Article ========== -->
-<div class="container p-0"><div class="row rounded-3 bg-light p-4 m-0 my-4 m-lg-4">
-
-<div class="col-md-5">
-<p class=" badge rounded-pill bg-info me-2 fs-4 px-3">課題1</p>
-<h2>映画と展覧会の鑑賞</h2>
-</div>
-
-<div class="col-md-7">
-<h4>課題の目的</h4>
-<p>
-これから大学で学ぶ映像やデザイン、アートの基礎知識をつけることを目的とします。批評的な視点を持って映画、展覧会を鑑賞して下さい。鑑賞した後の感動・想い・疑問点を言葉にして伝え、リテラシーを身につけることがこの課題のねらいです。
-</p>
-<p>
-たくさんの映像や、展示に触れること。これは、あなたが表現者として成長するためにはとても重要なことなのです。今までは何も気づけなかったことも、注意深く観察すると、先人たちの創意工夫、仕組みと目的が見えてくるはずです。
-</p>
-
-<h4>課題内容</h4>
-<p>映画3本、展覧会を3つ鑑賞してレポートを提出。それぞれのリストから選んで鑑賞してください。</p>
-
-<a class="btn btn-primary my-2" href="https://forms.gle/6BZs6UR1VHubVk4B9">映画・展覧会レポートの提出先</a><br>
-</a>
-
-
-
-
-
-</div>
-
-<!-- =================== -->
-<hr class="my-5">
-<div class="col-md-3">
-<h4>映画リスト</h4>
-</div>
-
-<div class="col-md-9 fs-sm ">
-<ul class="list-group list-group-flush lh-1 ">
-<li class="list-group-item ps-0">2001年宇宙の旅 （スタンリー・キューブリック）</li>
-<li class="list-group-item ps-0">ユージュアル・サスペクツ （ブライアン・シンガー）</li>
-<li class="list-group-item ps-0">アメリ （ジャン＝ピエール・ジュネ）</li>
-<li class="list-group-item ps-0">ジョーカー （トッド・フィリップス）</li>
-<li class="list-group-item ps-0">ONI ~ 神々山のおなり （堤大介）</li>
-<li class="list-group-item ps-0">太陽の塔 （関根光才）</li>
-<li class="list-group-item ps-0">気狂いピエロ （ジャン＝リュック・ゴダール）</li>
-<li class="list-group-item ps-0">雨月物語 （溝口健二）</li>
-<li class="list-group-item ps-0">E.T. （スティーブン・スピルバーグ）</li>
-<li class="list-group-item ps-0">ジョーズ （スティーブン・スピルバーグ）</li>
-<li class="list-group-item ps-0">東京物語 （小津安二郎 ）</li>
-<li class="list-group-item ps-0">友だちのうちはどこ？ （アッバス・キアロスタミ）</li>
-<li class="list-group-item ps-0">セバスチャン・サルガド - 地球へのラブレター （ヴィム・ヴェンダース）</li>
-<li class="list-group-item ps-0">サクリファイス （アンドレィ・タルコフスキー）</li>
-<li class="list-group-item ps-0">ブレードランナー （リドリー・スコット）</li>
-<li class="list-group-item ps-0">羅生門 （黒澤明）</li>
-<li class="list-group-item ps-0">七人の侍 （黒澤明）</li>
-<li class="list-group-item ps-0">JUNK HEAD（堀貴秀）</li>
-<li class="list-group-item ps-0">コックと泥棒その妻と愛人（ピーター・グリーナウェイ）</li>
-</ul>
-</div>
-
-<!-- =================== -->
-<hr class="my-5">
-<div class="col-md-3">
-<h4>展覧会リスト</h4>
-</div>
-
-<div class="col-md-9 fs-sm ">
-<ul class="list-group list-group-flush lh-1">
-<li class="list-group-item fw-bold ps-0">近隣の博物館・美術館・資料館など文化施設</li>
-<li class="list-group-item ps-0">または下記施設を推奨</li>
-</ul>
-
-
-<ul class="list-group list-group-flush lh-1 mt-3">
-<li class="list-group-item ps-0">弘前れんが倉庫美術館（青森県）</li>
-<li class="list-group-item ps-0">青森県立美術館（青森県）</li>
-<li class="list-group-item ps-0">十和田現代美術館（青森県）</li>
-<li class="list-group-item ps-0">寺山修司記念館（青森県）</li>
-<li class="list-group-item ps-0">秋田県立美術館（秋田県）</li>
-<li class="list-group-item ps-0">光原社（岩手県）</li>
-<li class="list-group-item ps-0">土門拳記念館（山形県）</li>
-<li class="list-group-item ps-0">せんだいメディアテーク（宮城県）</li>
-<li class="list-group-item ps-0">気仙沼アーク美術館（宮城県）</li>
-<li class="list-group-item ps-0">諸橋近代美術館（福島県）</li>
-<li class="list-group-item ps-0">21世紀美術館（石川県）</li>
-</ul>
-
-<ul class="list-group list-group-flush lh-1 mt-3">
-<li class="list-group-item ps-0">東京都写真美術館（東京都）</li>
-<li class="list-group-item ps-0">国立西洋美術館（東京都）</li>
-<li class="list-group-item ps-0">東京都現代美術館（東京都）</li>
-<li class="list-group-item ps-0">日本民藝館（東京都）</li>
-<li class="list-group-item ps-0">インターメディアテク（東京都）</li>
-<li class="list-group-item ps-0">NTTインターコミュニケーション・センター（東京都）</li>
-<li class="list-group-item ps-0">森美術館（東京都）</li>
-<li class="list-group-item ps-0">国立新美術館（東京都）</li>
-<li class="list-group-item ps-0">21_21 DESIGN SIGHT（東京都）</li>
-</ul>
-
-</ul>
-</div></div>
-
-
-<!-- ========= Article ========== -->
-<div class="container p-0"><div class="row rounded-3 bg-light p-4 m-0 my-4 m-lg-4">
-
-<div class="col-md-5">
-<p class=" badge rounded-pill bg-info me-2 fs-4 px-3">課題2</p>
-<h2>自己紹介ビデオの制作</h2>
-</div>
-
-<div class="col-md-7">
-<h4>課題の目的</h4>
-<p>
-大学の学びには何よりも自主性が求められます。そして自分の考えや想いを他者に伝えることも重要になります。この課題では映像で人に伝えることを「まず映像を作ってみる」という体験を通して考えるものです。
-</p>
-
-<h4>課題内容</h4>
-<p>同級生に向けた「自己紹介ビデオ」を 1分で制作して下さい。映像のクオリティよりも、自分のことを映像で伝えられるか、相手に興味を持ってもらえるかがポイントです。映像を学ぶ前段階の自主トレーニングと捉えて下さい。</p>
-<a class="btn btn-primary my-2" href="https://forms.gle/HZgV7TtiMV4B4NDg9">自己紹介ビデオの提出先
-</a>
-</div>
-
-<hr class="my-5">
-
-<div class="col">
-<h4>映像制作のポイント</h4>
-<ul class="list-group list-group-flush lh-1 mt-3">
-
-<li class="list-group-item ps-0">1. 好きな映像、興味があること、趣味、特技、将来の夢など自由にテーマを設定して下さい。(複数可) </li>
-<li class="list-group-item ps-0">2. 実写、アニメ、CG、写真(素材に使用する)など手法は自由です。</li>
-<li class="list-group-item ps-0">3. 音声を入れて下さい。(自分の声、環境音、音楽など自由)</li>
-<li class="list-group-item ps-0">4. 撮影カメラ、編集はスマートフォンでも可。自分に合ったアプリを使用してください。</li>
-<li class="list-group-item ps-0">5. 再生時間一分の映像とします。</li>
-
-</div>
-<hr class="my-5">
-
-
-
-</div></div>
 
 
 
